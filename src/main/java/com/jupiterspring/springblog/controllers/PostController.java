609bd9feea9bf93f03fc01a9097f51dad68ba82e
Matthew <matthew.d.baker20@gmail.com>
2021-02-11 21:10:05 -0600

    messing with style, error page, partials and set up for post dao and interface

--- src/main/java/com/jupiterspring/springblog/controllers/PostController.java
+++ src/main/java/com/jupiterspring/springblog/controllers/PostController.java
@@ -1,18 +1,26 @@
 package com.jupiterspring.springblog.controllers;
 import com.jupiterspring.springblog.model.Post;
+import com.jupiterspring.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
 public class PostController {
-    Post myPost = new Post(1, "Covid is a lie", "Quit letting this run your life and get back out there. The government does not always know what is best");
-    Post myPost2 = new Post(2, "Interaction", "Covid has reduced interaction with others, it is having a real impact on mental health");
-    Post myPost3 = new Post(3, "President Trump", "Trump won the election");
+    Post myPost = new Post(1, "Matt", "Covid is a lie", "Quit letting this run your life and get back out there. The government does not always know what is best", new Date(1999-12-12));
+    Post myPost2 = new Post(2, "Matt", "Interaction", "Covid has reduced interaction with others, it is having a real impact on mental health", new Date(1999-12-12));
+    Post myPost3 = new Post(3, "Matt", "President Trump", "Trump won the election", new Date(1999-12-12));
 
+
+    private final PostRepository postDao;
+
+    public PostController(PostRepository postDao){
+        this.postDao = postDao;
+    }
 
 
     @GetMapping("/post")
@@ -27,19 +35,24 @@ public class PostController {
     }
 
     @GetMapping("/post/{id}")
-    public String plainPost(@PathVariable int id){
-
+    public String plainPost(@PathVariable long id, Model model){
+        Post myPost = postDao.getOne(id);
+        model.addAttribute("myPost", myPost);
         return "";
     }
 
     @GetMapping("/post/create")
     public String createForm(){
-        return "";
+
+        return "posts/createPost";
     }
 
     @PostMapping("/post/create")
     public String createPost(){
-        return "";
+//        Post post = new Post();
+//
+//        postDao.save(post);
+        return "posts/index";
     }
 
 }
--- src/main/java/com/jupiterspring/springblog/model/Post.java
+++ src/main/java/com/jupiterspring/springblog/model/Post.java
@@ -1,17 +1,35 @@
 package com.jupiterspring.springblog.model;
+import javax.persistence.*;
+import java.util.Date;
 
+@Entity
+@Table(name = "posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, unique = true)
     private String title;
+
+    @Column(nullable = false)
     private String body;
+
+    @Column(columnDefinition = "varchar(100) default 'Unknown'")
+    private String author;
+
+    @Column(nullable = false)
+    private Date date;
 
     public Post(){}
 
-    public Post(long id, String t, String b){
+    public Post(long id, String a, String t, String b, Date d){
         this.id = id;
+        this.author = a;
         this.title = t;
         this.body = b;
+        this.date = d;
     }
 
     public String getTitle() {
@@ -36,5 +54,13 @@ public class Post {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
